9217796a0c8a56c7e23560b0b4a84c5e3b745655
gde-alme <goncalopes98@gmail.com>
2023-04-13 14:15:14 +0100

    added mem allocation ex to abstract_c

--- abstraction_in_c/simpleAPI.c
+++ abstraction_in_c/simpleAPI.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-struct s_api {
+/**
+ *	simpleAPI:
+ *	@ft_hello:	print msg
+ *	@ft_bye:	print msg
+ *	@s_api:		api struct
+*/
+typedef struct s_api {
 	void	(*hello)(void);
 	void	(*bye)(void);
-};
+}t_api;
 
 void ft_hello() {
 	printf("sup\n");
@@ -13,14 +20,27 @@ void ft_bye() {
 	printf("hala\n");
 }
 
-struct s_api my_api = {
+t_api	*_initApi() {
+	t_api	*api_object = (t_api*)malloc(sizeof(t_api));
+	
+	api_object->hello	= ft_hello;
+	api_object->bye		= ft_bye;
+
+	return (api_object);
+}
+
+t_api myApi = {
 	.hello	= ft_hello,
 	.bye	= ft_bye
 };
 
 int main() {
-
-	my_api.hello();
+	//example from book
+	myApi.hello();
+	
+	//example allocating memmory
+	t_api	*myApi2 = _initApi();
+	myApi2->bye();
 
 	return (0);
 }
